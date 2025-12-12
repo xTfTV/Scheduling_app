a618f9104c6c9c585ff47c81a927ad519df80ad7
xTff <mscott2003@gmail.com>
2025-12-12 10:28:07 -0500

    Creating the APIs for the table connections

--- server.js
+++ server.js
@@ -6,9 +6,38 @@ const db = require('./db');
 const app = express();
 const PORT = process.env.PORT || 3000; // Needs to be different than 3306 (XAMPP default)
 
-// Testing a route for the DB connection
-app.get('/users', (req, res) => {
-    db.query('SELECT * FROM users', (err, results) => {
+const path = require('path');
+
+// Serving the files
+app.use(express.static(path.join(__dirname, 'HTML')));
+app.use('/Styles', express.static(path.join(__dirname, 'Styles')));
+app.use('/JS', express.static(path.join(__dirname, 'JS')));
+
+
+
+// Creating the API to send and retrieve the users
+app.get('/API/users', (req, res) => {
+    db.query('SELECT * FROM user_table', (err, results) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.json(results);
+    });
+});
+
+// Creating the API to send and retrieve deliveries
+app.get('/API/deliveries', (req, res) => {
+    db.query('SELECT * FROM deliveries_table', (err, results) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.json(results);
+    });
+});
+
+// Creating the API to send and retrieve customer information
+app.get('/API/customers', (req, res) => {
+    db.query('SELECT * FROM customer_info', (err, results) => {
         if (err) {
             return res.status(500).send(err);
         }
